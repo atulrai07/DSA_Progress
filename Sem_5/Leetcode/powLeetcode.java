d00f106d6dca088c32d8356e5d54d122136c1a97
atulrai07 <atulrai.varanasi@gmail.com>
2026-01-02 23:46:01 +0530

    pow

---
+++ Sem_5/Leetcode/powLeetcode.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+public class powLeetcode {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return Pow(x, N);
+    }
+    
+    public double Pow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = Pow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        powLeetcode solution = new powLeetcode();
+        double x = 2.0;
+        int n = 10;
+        double result = solution.myPow(x, n);
+        System.out.println("Result: " + result);
+    }
+}
